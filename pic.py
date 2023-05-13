76317a90cb2997f5d6e3a1c3ed0c94d4fda63d4c
nico <85696452+wilsnico240@users.noreply.github.com>
2023-05-13 20:05:35 +0200

    Update pic.py -- chatgpt image creator script

--- pic.py
+++ pic.py
@@ -2,8 +2,9 @@ import sys
 import openai
 import webbrowser
 import requests
+import random
 
-openai.api_key="     "
+openai.api_key="    "
 
 def img_gen(query):
     response = openai.Image.create(
@@ -21,26 +22,28 @@ open_conditions = ("open")
 new_conditions = ("new")
 
 while True:
-     query= input("Enter the desired keywords separated by a comma to generate the image, or type 'exit' followed by ENTER to quit: ")
-     if query in exit_conditions:
-         sys.exit()
+    query= input("Enter the desired keywords separated by a comma to generate the image, or type 'exit' followed by ENTER to quit: ")
+    if query in exit_conditions:
+        sys.exit()
 
-     url = img_gen(query)
+    url = img_gen(query)
 
-     while True:
-         query = input("Type 'open' followed by ENTER to view the image in browser, 'get' followed by ENTER to download the generated image, 'print' followed by ENTER to show the URL, 'new' followed by ENTER to generate a new image or 'exit' followed by ENTER to quit: ")
-         if query in exit_conditions:
-             sys.exit()
-         if query in get_conditions:
-             response = requests.get(url)
-             with open('picgpt.png', 'wb') as f:
-                 f.write(response.content)
-             print("The image 'picgpt' is downloaded in png format on your computer !!!")
-         if query in print_conditions:
-             print(url)
-         if query in open_conditions:
-             webbrowser.open(url)
-         if query in new_conditions:
-             break
-         else:
-             print("    ")
+    while True:
+        query = input("Type 'open' followed by ENTER to view the image in browser, 'get' followed by ENTER to download the generated image, 'print' followed by ENTER to show the URL, 'new' followed by ENTER to generate a new image, or 'exit' followed by ENTER to quit: ")
+        if query in exit_conditions:
+            sys.exit()
+        if query in get_conditions:
+            random_number = str(random.randint(0,1000)) # generate random number between 0 and 1000
+            filename = "picgpt" + random_number + ".png" # create filename with random number
+            response = requests.get(url)
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            print("Image downloaded as", filename)
+        if query in print_conditions:
+            print(url)
+        if query in open_conditions:
+            webbrowser.open(url)
+        if query in new_conditions:
+            break
+        else:
+            print("    ")
