f56ccfa151ef0d5f2bad6e964ef93162e92c77ae
nico <85696452+wilsnico240@users.noreply.github.com>
2023-05-13 20:15:46 +0200

    Update  picfr.py - script de création d'images pour chatgpt

--- picfr.py
+++ picfr.py
@@ -2,6 +2,7 @@ import sys
 import openai
 import webbrowser
 import requests
+import random
 
 openai.api_key="    "
 
@@ -13,7 +14,7 @@ def img_gen(query):
     )
     return response['data'][0]['url']
 
-# main program
+# programme principal
 exit_conditions = ("exit")
 get_conditions = ("get")
 print_conditions = ("print")
@@ -21,26 +22,28 @@ open_conditions = ("open")
 new_conditions = ("new")
 
 while True:
-     query= input("Entrez les mots clés souhaités séparés par une virgule pour générer l'image, ou tapez 'exit' suivi de ENTER pour quitter : ")
-     if query in exit_conditions:
-         sys.exit()
+    query= input("Entrez les mots clés souhaités séparés par une virgule pour générer l'image, ou tapez 'exit' suivi de ENTER pour quitter : ")
+    if query in exit_conditions:
+        sys.exit()
 
-     url = img_gen(query)
+    url = img_gen(query)
 
-     while True:
-         query = input("Tapez 'open' suivi de ENTER pour afficher l'image dans le navigateur, 'get' suivi de ENTER pour télécharger l'image générée, 'print' suivi de ENTER pour afficher l'URL, 'new' suivi de ENTER pour générer une nouvelle image ou ' exit' suivi de ENTER pour quitter :")
-         if query in exit_conditions:
-             sys.exit()
-         if query in get_conditions:
-             response = requests.get(url)
-             with open('picgpt.png', 'wb') as f:
-                 f.write(response.content)
-             print("L'image 'picgpt' télécharge au format png sur votre ordinateur !!!")
-         if query in print_conditions:
-             print(url)
-         if query in open_conditions:
-             webbrowser.open(url)
-         if query in new_conditions:
-             break
-         else:
-             print("    ")
+    while True:
+        query = input("Tapez 'open' suivi de ENTER pour afficher l'image dans le navigateur, 'get' suivi de ENTER pour télécharger l'image générée, 'print' suivi de ENTER pour afficher l'URL, 'new' suivi de ENTER pour générer une nouvelle image ou ' exit' suivi de ENTER pour quitter :")
+        if query in exit_conditions:
+            sys.exit()
+        if query in get_conditions:
+            random_number = str(random.randint(0,1000)) # générer un nombre aléatoire entre 0 et 1000
+            filename = "picgpt" + random_number + ".png" # créer un nom de fichier avec un nombre aléatoire
+            response = requests.get(url)
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+            print("Image téléchargée en tant que", filename)
+        if query in print_conditions:
+            print(url)
+        if query in open_conditions:
+            webbrowser.open(url)
+        if query in new_conditions:
+            break
+        else:
+            print("    ")
