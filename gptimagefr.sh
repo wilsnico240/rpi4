5f2992222332d33b03c2192915d80a0fc9c43352
nico <85696452+wilsnico240@users.noreply.github.com>
2023-05-14 23:23:53 +0200

    gptimagefr.sh -- script de création d'images pour chatgpt

--- gptimagefr.sh
+++ gptimagefr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Enter the desired keywords separated by a comma to generate the image, or type 'exit' followed by ENTER to quit:"
+echo "Tapez 'open' suivi de ENTER pour afficher l'image dans le navigateur, 'get' suivi de ENTER pour télécharger l'image générée, 'print' suivi de ENTER pour afficher l'URL, 'new' suivi de ENTER pour générer une nouvelle image ou ' exit' suivi de ENTER pour quitter:"
 read -r query
 
 while [ "$query" != "exit" ]
 do
     url=$(python3 - <<END
 import openai
-openai.api_key="   "
+openai.api_key="     "
 def img_gen(query):
     response = openai.Image.create(
         prompt=query,
@@ -19,7 +19,8 @@ print(img_gen("$query"))
 END
 )
 
-    echo "Tapez 'open' suivi de ENTER pour afficher l'image dans le navigateur, 'get' suivi de ENTER pour télécharger l'image générée, 'print' suivi de ENTER pour afficher l'URL, 'new' suivi de ENTER pour générer une nouvelle image ou ' exit' suivi de ENTER pour quitter:"
+
+    echo "Tapez 'open' suivi de ENTER pour afficher l'image dans le navigateur, 'get' suivi de ENTER pour télécharger l'image générée, 'print' suivi de ENTER pour afficher l'URL, 'new' suivi de ENTER pour générer une nouvelle image ou ' exit' suivi de ENTER pour quitter :"
     read -r option
 
     while [ "$option" != "new" ]
@@ -32,7 +33,7 @@ END
             random_number=$(shuf -i 0-1000 -n 1)
             filename="picgpt$random_number.png"
             curl -o "$filename" "$url"
-            echo "Image downloaded as $filename"
+            echo "Image téléchargée en tant que $filename"
         elif [ "$option" = "print" ]
         then
             echo "$url"
@@ -40,19 +41,19 @@ END
         then
             xdg-open "$url"
         else
-            echo "Invalid option. Please try again."
+            echo "Option invalide. Veuillez réessayer."
         fi
 
     echo " "
     echo " "
 
         echo "Tapez 'open' suivi de ENTER pour afficher l'image dans le navigateur, 'get' suivi de ENTER pour télécharger l'image générée, 'print' suivi de ENTER pour afficher l'URL, 'new' suivi de ENTER pour générer une nouvelle image ou ' exit' suivi de ENTER pour quitter :"
-        read -r option
+         read -r option
     done
 
     echo " "
     echo " "
 
-    echo "Entrez les mots clés souhaités séparés par une virgule pour générer l'image, ou tapez 'exit' suivi de ENTER pour quitter :"
-    read -r query
+    echo "Tapez 'open' suivi de ENTER pour afficher l'image dans le navigateur, 'get' suivi de ENTER pour télécharger l'image générée, 'print' suivi de ENTER pour afficher l'URL, 'new' suivi de ENTER pour générer une nouvelle image ou ' exit' suivi de ENTER pour quitter:"
+        read -r query
 done
