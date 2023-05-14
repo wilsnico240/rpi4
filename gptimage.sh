f90c03f4a22f73e981dabfe5c5a12ec526955798
nico <85696452+wilsnico240@users.noreply.github.com>
2023-05-14 23:00:40 +0200

    gptimage.sh  -- chatgpt image creator script

---
+++ gptimage.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo "Enter the desired keywords separated by a comma to generate the image, or type 'exit' followed by ENTER to quit:"
+read -r query
+
+while [ "$query" != "exit" ]
+do
+    url=$(python3 - <<END
+import openai
+openai.api_key="     "
+def img_gen(query):
+    response = openai.Image.create(
+        prompt=query,
+        n=1,
+        size="1024x1024"
+    )
+    return response['data'][0]['url']
+print(img_gen("$query"))
+END
+)
+
+    echo "Type 'open' followed by ENTER to view the image in browser, 'get' followed by ENTER to download the generated image, 'print' followed by ENTER to show the URL, 'new' followed by ENTER to generate a new image, or 'exit' followed by ENTER to quit:"
+    read -r option
+
+    while [ "$option" != "new" ]
+    do
+        if [ "$option" = "exit" ]
+        then
+            exit 0
+        elif [ "$option" = "get" ]
+        then
+            random_number=$(shuf -i 0-1000 -n 1)
+            filename="picgpt$random_number.png"
+            curl -o "$filename" "$url"
+            echo "Image downloaded as $filename"
+        elif [ "$option" = "print" ]
+        then
+            echo "$url"
+        elif [ "$option" = "open" ]
+        then
+            xdg-open "$url"
+        else
+            echo "Invalid option. Please try again."
+        fi
+
+    echo " "
+    echo " "
+
+        echo "Type 'open' followed by ENTER to view the image in browser, 'get' followed by ENTER to download the generated image, 'print' followed by ENTER to show the URL, 'new' followed by ENTER to generate a new image, or 'exit' followed by ENTER to quit:"
+        read -r option
+    done
+
+    echo " "
+    echo " "
+
+    echo "Enter the desired keywords separated by a comma to generate the image, or type 'exit' followed by ENTER to quit:"
+    read -r query
+done
