1d5004857f2e5b1fa7d275a3f72310862a29c6fa
nico <85696452+wilsnico240@users.noreply.github.com>
2023-05-19 22:32:43 +0200

    chatbotespeakfr.py -- chatgpt gui +tts espeak

--- chatbotespeakfr.py
+++ chatbotespeakfr.py
@@ -12,49 +12,72 @@ def get_response(prompt):
         n=1,
         stop=None,
         temperature=0.4,
-     )
-  
+    )
     message = response.get('choices')[0].get('text')
     return message
 
 def say_response(message):
-    subprocess.Popen(['espeak-ng', '-v', 'fr+f2', '-m', '-s', '127', '-z', message], stdout=subprocess.PIPE)
+    subprocess.Popen(['espeak-ng', '-v', voice.get(), '-m', '-s', speed.get(), '-z', message], stdout=subprocess.PIPE)
 
 def send_message():
-    message = entry_box.get("1.0",'end-1c').strip()
-    entry_box.delete("0.0", tk.END)
+    message = entry_box.get("1.0",'end-1c')
+    response = get_response(message)
+    chat_history.config(state=tk.NORMAL)
+    chat_history.insert(tk.END, "Vous: " + message + "\n\n")
+    chat_history.config(foreground="#442265", font=("Verdana", 12 ))
+    chat_history.insert(tk.END, "Assistante: " + response + "\n\n")
+    chat_history.config(state=tk.DISABLED)
+    chat_history.yview(tk.END)
+    say_response(response)
+    entry_box.delete('1.0', tk.END)
 
-    if message != '':
-        chat_box.config(state=tk.NORMAL)
-        chat_box.insert(tk.END, "Vous: " + message + '\n\n')
-        chat_box.config(foreground="#442265", font=("Verdana", 12 ))
+app = tk.Tk()
+app.title("ChatGPT")
+app.geometry("750x500")
 
-        response = get_response(message)
-        chat_box.insert(tk.END, "Assistante: " + response + '\n\n')
-        chat_box.config(state=tk.DISABLED)
+voice_label = tk.Label(app, text="Sélectionner la voix:")
+voice_label.pack()
 
-        say_response(response)
+voice_options = ["aucune", "fr+f2", "fr+m7", "en+f2", "en+m7", "nl+f4", "nl+m7", "de+f2", "de+m7"]
+voice = tk.StringVar(app)
+voice.set(voice_options[0])
 
-root = tk.Tk()
-root.title("ChatGPT")
-root.geometry("750x500")
+voice_dropdown = tk.OptionMenu(app, voice, *voice_options)
+voice_dropdown.pack()
 
-chat_box = tk.Text(root, bd=0, bg="white", height="8", width="50", font="Arial",)
+speed_label = tk.Label(app, text="Sélectionner la vitesse:")
+speed_label.pack()
 
-chat_box.config(state=tk.DISABLED)
+speed = tk.StringVar(app)
+speed.set("127")
 
-scrollbar = tk.Scrollbar(root, command=chat_box.yview, cursor="heart")
-chat_box['yscrollcommand'] = scrollbar.set
+speed_scale = tk.Scale(app, variable=speed, from_=80, to=400, orient=tk.HORIZONTAL)
+speed_scale.pack()
 
-send_button = tk.Button(root, font="Arial", text="Envoyer", width="10", height=5,
+chat_history = tk.Text(app, bd=1, bg='white', height=15, width=50)
+chat_history.pack()
+
+scrollbar = tk.Scrollbar(app)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+chat_history.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=chat_history.yview, cursor="heart")
+
+entry_box = tk.Text(app, bd=0, bg="white", width="28", height="5", font="Arial")
+entry_box.pack()
+
+send_button = tk.Button(app, font="Arial", text="Envoyer", width="10", height=5,
                         bd=0, bg="blue", activebackground="#ffffff",
                         command=send_message)
+send_button.pack()
 
-entry_box = tk.Text(root, bd=0, bg="white", width="28", height="5", font="Arial")
-
-scrollbar.place(x=726,y=6, height=386)
-chat_box.place(x=6,y=6, height=386, width=720)
+voice_label.place(x=116,y=10)
+voice_dropdown.place(x=255,y=5)
+speed_label.place(x=355,y=10)
+speed_scale.place(x=513,y=2)
+chat_history.place(x=6,y=50, height=352, width=724)
+scrollbar.place(x=730,y=50, height=353)
 entry_box.place(x=128, y=401, height=90, width=615)
 send_button.place(x=6, y=401, height=90)
 
-root.mainloop()
+app.mainloop()
