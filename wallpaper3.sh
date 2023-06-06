a674c2df59f159fb164bef8bb2aaccc0669f8519
nico <85696452+wilsnico240@users.noreply.github.com>
2023-06-06 09:44:54 +0200

    wallpaper3.sh - Central Europe satellite rain image, 5 min interval download wallpaper script.

--- wallpaper3.sh
+++ wallpaper3.sh
@@ -2,7 +2,7 @@
 
 interval=300 # 5 * 60
 
-url="https://api.sat24.com/animated/EU/rainTMC/3/Central%20European%20Standard%20Time/155296"
+url="https://api.sat24.com/animated/EU/rainTMC/3/Romance%20Standard%20Time/4506700"
 
 while true
 do
