c1b03ab164666390904741db939ef23449cdea52
nico <85696452+wilsnico240@users.noreply.github.com>
2023-06-06 09:16:58 +0200

    wallpaper.sh - Central Europe satellite image, 5 min interval download wallpaper script

---
+++ wallpaper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+interval=300 # 5 * 60
+
+url="https://api.sat24.com/animated/EU/infraPolair/3/Romance%20Standard%20Time/4575796"
+
+while true
+do
+    response=$(curl -o wallpaper.gif -w '%{http_code}' "$url")
+    if [ "$response" -eq 200 ]
+    then
+        echo "Image download @:" $(date +"%Y-%m-%d %H:%M:%S")
+    else
+        echo "Error download image."
+    fi
+
+    sleep "$interval"
+done
