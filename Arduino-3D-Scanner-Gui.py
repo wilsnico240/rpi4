ecbf2cdd0456d3600f81a1a88431131b35a3bf88
nico <85696452+wilsnico240@users.noreply.github.com>
2024-05-02 16:28:01 +0200

    Arduino-3D-Scanner-Gui.py

--- Arduino-3D-Scanner-Gui.py
+++ Arduino-3D-Scanner-Gui.py
@@ -16,23 +16,43 @@ button_frame.pack()
 ser = serial.Serial('/dev/ttyACM0', 9600)
 
 def ontvang_data():
-     while True:
-         data = ser.readline().decode().strip()
-         data = data.replace(" ", ",")
-         textbox.insert(tk.END, data + '\n')
+    while True:
+        data = ser.readline().decode().strip()
+        data = data.replace(" ", ",")
+        textbox.insert(tk.END, data + '\n')
 
 def reset_textbox():
-     textbox.delete("1.0", tk.END)
+    textbox.delete("1.0", tk.END)
 
 def opslaan():
-     bestand = open("3D-Scan.txt", "w")
-     bestand.write(textbox.get("1.0", tk.END))
-     bestand.close()
+    bestand = open("3D-Scan.txt", "w")
+    bestand.write(textbox.get("1.0", tk.END))
+    bestand.close()
 
 def close_window():
-     ser.close()
-     root.quit()
-     root.destroy()
+    ser.close()
+    root.quit()
+    root.destroy()
+
+def calculate():
+    input_data = textbox.get("1.0", tk.END)
+    lines = input_data.split("\n")
+    output_data = []
+
+    for line in lines:
+        line_data = line.split(",")
+        new_line_data = []
+        for i, datum in enumerate(line_data):
+            try:
+                new_data = 100 - float(datum)
+                new_line_data.append("{:.0f}".format(new_data))
+            except ValueError:
+                new_line_data.append(datum)
+        output_data.append(",".join(new_line_data))
+
+    output_text = "\n".join(output_data)
+    textbox.delete("1.0", tk.END)
+    textbox.insert(tk.END, output_text)
 
 sluitknop = tk.Button(button_frame, text="Close", command=close_window)
 sluitknop.pack(side=tk.LEFT)
@@ -42,6 +62,9 @@ reset_knop.pack(side=tk.LEFT)
 
 opslaan_knop = tk.Button(button_frame, text="Save", command=opslaan)
 opslaan_knop.pack(side=tk.LEFT)
+
+calculate_knop = tk.Button(button_frame, text="Calculate", command=calculate)
+calculate_knop.pack(side=tk.LEFT)
 
 receive_thread = threading.Thread(target=ontvang_data)
 receive_thread.start()
